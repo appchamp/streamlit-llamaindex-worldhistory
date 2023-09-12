c76c4644f95d30c72c2654da506e8e2d9721916c
App Champ <appenginechamp>
2023-09-12 04:42:12 -0700

    working app2 with 1 doc

--- app2/streamlit_app.py
+++ app2/streamlit_app.py
@@ -10,15 +10,20 @@ from llama_index import (
 )
 from langchain.chat_models import ChatOpenAI
 
-index_name = "./saved_index_wh"
-documents_folder = "./docs/world-history-txt"
+# one document only
+index_name = "../saved_index_wh_1"
+documents_folder = "../docs/world-history-txt-1"
 
 @st.cache_resource
 def initialize_index(index_name, documents_folder):
     llm_predictor = LLMPredictor(
-        llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     )
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+
+    # use a local model instead
+    # service_context = ServiceContext.from_defaults(embed_model="local")
+
     if os.path.exists(index_name):
         st.status("Found existing index on disk.", state="complete")
 
@@ -29,8 +34,9 @@ def initialize_index(index_name, documents_folder):
     else:
         with st.status("No exiting index found.. creating index..", expanded=True, state="running") as status:
             documents = SimpleDirectoryReader(documents_folder).load_data()
+            status.update(label=f"Reading {len(documents)} file(s)..", state="running")
             index = GPTVectorStoreIndex.from_documents(
-                documents, service_context=service_context
+                documents, service_context=service_context, show_progress=True
             )
             status.update(label="Index creation DONE !", expanded=False, state="complete")
         index.storage_context.persist(persist_dir=index_name)
@@ -38,22 +44,19 @@ def initialize_index(index_name, documents_folder):
     return index
 
 
-@st.cache_data(persist=True)
+@st.cache_data(max_entries=200, persist=True)
 def query_index(_index, query_text):
     if _index is None:
         return "Please initialize the index!"
-    response = _index.as_query_engine().query(query_text)
+    response = _index.as_query_engine(verbose='True').query(query_text)
     return str(response)
 
 
 st.title("World History - LlamaIndex 🦙")
-st.header("Welcome to World History.")
-st.write(
-    "Your query will be answered using the essay as context, using embeddings from text-ada-002 and LLM completions from gpt-3.5-turbo."
-)
+st.header("Welcome to World History")
+st.caption('''Using [1 doc](https://apcentral.collegeboard.org/media/pdf/ap22-cr-report-world-history-modern.pdf "ap22-cr-report-world-history-modern.pdf"):''')
 
 index = None
-
 if st.secrets.has_key("openai_key"):
     api_key = st.secrets.openai_key
     st.status("Using existing API Key from st.secrets", state="complete")
@@ -70,7 +73,13 @@ if api_key:
 if index is None:
     st.warning("Please enter your api key first.")
 
-text = st.text_input("Query text:", value="What do you know about Canton factory?")
+
+st.markdown('''
+            Try one of these queries (or your own):  
+            - Who owned the diamond empire?  
+            - what do you know about the incan empire?
+        ''')
+text = st.text_input("Query text:", value="Who is Biran?")
 
 if st.button("Run Query") and text is not None:
     response = query_index(index, text)
