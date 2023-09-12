6a09853beb53d790415e2bcbfe0d1c980779ec91
App Champ <appenginechamp>
2023-09-12 02:20:52 -0700

    move app1 to app1 dir

---
+++ app1/streamlit_app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from llama_index import (
+    SimpleDirectoryReader, 
+    VectorStoreIndex, 
+    ServiceContext, 
+    Document,)
+
+from llama_index.llms import OpenAI
+import openai
+
+st.set_page_config(page_title="WorldHistory QA powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("World History QA, powered by LlamaIndex 💬🦙")
+
+if st.secrets.has_key(openai_key):
+    openai.api_key = st.secrets.openai_key
+else:
+    openai.api_key = st.sidebar.text_input(
+        label="#### Your OpenAI API key 👇",
+        placeholder="Paste your openAI API key, sk-",
+        type="password")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about AP World History"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext .from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="Act as if you are preparing questions for AP World history exam. Create a question on the Global Tapestry Unit of the AP World history. The question should be focused on the East Asia region. The question should make students demonstrate understanding the major themes and content areas covered within a given unit. The question should require students to demonstrate their understanding of the material through analysis and interpretation, rather than simply recalling facts. Assign a difficulty level for the question, where the difficulty can vary from 1 to 10 and return it in the \"difficulty\" parameter."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+chat_engine = index.as_chat_engine(chat_mode="context", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
